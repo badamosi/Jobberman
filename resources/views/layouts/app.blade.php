308027381e5b537a3244bc5da221934c1400de9b
Badamosi Majeed <badamosimajeed@gmail.com>
2021-02-27 16:19:13 +0100

    logo update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,7 +24,9 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                <img src="https://www.jobberman.com/build/static-assets/img/ng/landscape-86f8ce6fb7.svg" alt="AdminLTE Logo"
+                        class="brand-image" style="opacity: .8">
+
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
